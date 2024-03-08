d6bc426bf5c781c52b42b68b95e98839cd2e5704
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-03-08 07:33:05 +0700

    update test order

--- tester/script-static-singlereq.sh
+++ tester/script-static-singlereq.sh
@@ -8,45 +8,44 @@ pause_test() {
   sleep 60;
 }
 
-echo "starting test using wrk..."
-wrk -c 1000 -t 8 --timeout 1s --latency -d "${DEFAULT_DURATION}s" $TARGET_URL/wrk
+echo "===============================\nstarting test using vegeta..."
+echo "GET $TARGET_URL/vegeta" | vegeta attack -rate 0 -max-workers 10000 -duration "${DEFAULT_DURATION}s"  | vegeta report -type=text
 
 pause_test
 
-echo "starting test using autocannon..."
+echo "===============================\nstarting test using plt..."
+$(go env GOPATH)/bin/plt --concurrency 1000 --duration="${DEFAULT_DURATION}s"  curl -X GET "$TARGET_URL/plt"
+
+pause_test
+
+echo "===============================\nstarting test using k6..."
+k6 run tester/k6/script.js
+
+pause_test
+
+echo "===============================\nstarting test using cassowary..."
+cassowary run -u $TARGET_URL/cassowary -c 50 -d "${DEFAULT_DURATION}"
+
+pause_test
+
+echo "===============================\nstarting test using drill..."
+drill --benchmark tester/drill/script.yaml
+
+pause_test
+
+#echo "===============================\nstarting test using artilerry..."
+#artillery run tester/artillery/script.yaml
+
+#pause_test
+
+echo "===============================\nstarting test using autocannon..."
 # we dont specify num of workers as IME it actually reduce the load
 node tester/autocannon/script-static-singlereq.js
 
 pause_test
 
-echo "starting test using vegeta..."
-echo "GET $TARGET_URL/vegeta" | vegeta attack -rate 0 -duration "${DEFAULT_DURATION}s"  | vegeta report -type=text
+echo "===============================\nstarting test using wrk..."
+wrk -c 1000 -t 8 --timeout 1s --latency -d "${DEFAULT_DURATION}s" $TARGET_URL/wrk
 
 pause_test
 
-echo "starting test using plt..."
-$(go env GOPATH)/bin/plt --concurrency 50 --duration="${DEFAULT_DURATION}s"  curl -X GET "$TARGET_URL/plt"
-
-pause_test
-
-echo "starting test using k6..."
-k6 run tester/k6/script.js
-
-pause_test
-
-echo "starting test using cassowary..."
-cassowary run -u $TARGET_URL/cassowary -c 50 -d "${DEFAULT_DURATION}"
-
-pause_test
-
-echo "starting test using drill..."
-drill --benchmark tester/drill/script.yaml
-
-pause_test
-
-#echo "starting test using artilerry..."
-#artillery run tester/artillery/script.yaml
-
-#pause_test
-
-pause_test
