266bdf8e25680427279ab2ee47f666135402fceb
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-03-09 12:43:59 +0700

    parameterize wrapper script

---
+++ tester/env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export TARGET_URL=http://target:3000/test
+export DEFAULT_DURATION="60"
+export NUM_THREAD=8
+export NUM_CONNECTIONS=1000
+export SLEEP_DURATION=60
--- tester/script-static-singlereq.sh
+++ tester/script-static-singlereq.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-export TARGET_URL=http://target:3000/test
-export DEFAULT_DURATION="60"
+source ./env.sh
 
 pause_test() {
-  echo "pausing for 60 seconds...";
-  sleep 60;
+  echo "pausing for $SLEEP_DURATION seconds...";
+  sleep $SLEEP_DURATION;
 }
 
 echo "===============================\nstarting test using vegeta..."
-echo "GET $TARGET_URL/vegeta" | vegeta attack -rate 0 -max-workers 10000 -duration "${DEFAULT_DURATION}s"  | vegeta report -type=text
+echo "GET $TARGET_URL/vegeta" | vegeta attack -rate 0 -max-workers $NUM_CONNECTIONS -duration "${DEFAULT_DURATION}s"  | vegeta report -type=text
 
 pause_test
 
 echo "===============================\nstarting test using plt..."
-$(go env GOPATH)/bin/plt --concurrency 1000 --duration="${DEFAULT_DURATION}s"  curl -X GET "$TARGET_URL/plt"
+$(go env GOPATH)/bin/plt --concurrency $NUM_CONNECTIONS --duration="${DEFAULT_DURATION}s"  curl -X GET "$TARGET_URL/plt"
 
 pause_test
 
@@ -45,7 +44,7 @@ node tester/autocannon/script-static-singlereq.js
 pause_test
 
 echo "===============================\nstarting test using wrk..."
-wrk -c 1000 -t 8 --timeout 1s --latency -d "${DEFAULT_DURATION}s" $TARGET_URL/wrk
+wrk -c $NUM_CONNECTIONS -t $NUM_THREAD --timeout 1s --latency -d "${DEFAULT_DURATION}s" $TARGET_URL/wrk
 
 pause_test
 
