6aab21f6f133f4fb5ff2a6ab79703b74eb1e4b29
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-04-09 06:32:05 +0700

    fix sleep duration setting

--- tester/env.sh
+++ tester/env.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-pause_test() {
-  echo "pausing for 60 seconds...";
-  sleep 60;
-}
-
 export TARGET_URL=http://target:3000/test
 export DEFAULT_DURATION="60"
 export NUM_THREAD=8
 export NUM_CONNECTIONS=1000
 export SLEEP_DURATION=60
 export NODE_PATH=$(npm root -g)
+
+pause_test() {
+  echo "pausing for $SLEEP_DURATION seconds...";
+  sleep $SLEEP_DURATION;
+}
 
 export PLT_CONCURRENCY_MULTIPLIER=5 # arbitrary number based on experiments to achieve max throughput
 export PLT_CONCURRENCY=$(($NUM_THREAD * $PLT_CONCURRENCY_MULTIPLIER))
