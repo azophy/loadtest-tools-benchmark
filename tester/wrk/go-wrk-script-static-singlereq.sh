96018ef5953f842f954f32c0594c0d7dd5f0fe50
Abdurrahman Shofy Adianto <azophy@gmail.com>
2025-01-02 23:06:26 +0700

    fix cli arguments

--- tester/wrk/go-wrk-script-static-singlereq.sh
+++ tester/wrk/go-wrk-script-static-singlereq.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-go-wrk -c $NUM_CONNECTIONS \
-       -T 1s \
-       -d "${DEFAULT_DURATION}s" \
-       $TARGET_URL/gowrk/static-singlereq
+$(go env GOPATH)/bin/go-wrk -c $NUM_CONNECTIONS \
+                            -T 1000 \
+                            -d $DEFAULT_DURATION \
+                            $TARGET_URL/gowrk/static-singlereq
 
